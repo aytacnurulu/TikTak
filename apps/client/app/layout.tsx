d26ae61690072f4082effb282348fd33846c66a6
Babek Ceferov <ceferovbabek07@gmail.com>
2026-07-25 05:39:03 +0400

    LandingPage 60% complited

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,14 +1,15 @@
 import Header from "@/shared/components/Header";
+import Footer from "@/shared/components/Footer";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import Container from "@/shared/components/Container";
+import Providers from "./providers";
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
   variable: "--font-roboto",
 });
-
 
 export default function RootLayout({
   children,
@@ -18,10 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Header />
-        <main>
-          <Container>{children}</Container>
-        </main>
+        <Providers>
+          <Header />
+          <main>
+            <Container>{children}</Container>
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,1 +1,45 @@
 export {};
+export interface ApiResponse<T> {
+  message: string;
+  data: T;
+  result: boolean;
+}
+
+export interface Pagination {
+  next: number | null;
+  prev: number | null;
+  current: number;
+  total: number;
+  totalPages: number;
+}
+
+export interface PaginatedResponse<T> extends ApiResponse<T[]> {
+  pagination: Pagination;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  img_url: string;
+  description: string;
+  created_at: string;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  img_url: string;
+  description: string;
+  price: string;
+  type: string;
+  created_at: string;
+  category: Pick<Category, "id" | "name">;
+}
+
+export interface Campaign {
+  id: number;
+  title: string;
+  description: string | null;
+  img_url: string | null;
+  created_at: string;
+}
